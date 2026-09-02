0b56d115567f5bd36e8a4b43823fe31bfbad2b84
Josh Bello <Jb230913@live.com>
2026-09-02 16:49:08 +0100

    update - change filename for index()

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -11,7 +11,7 @@ class ContactController extends Controller
      */
     public function index()
     {
-        return view('contacts.book');
+        return view('contacts.index');
     }
 
     /**
