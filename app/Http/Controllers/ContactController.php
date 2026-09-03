52572a0cdecbd161731c3b2bc746c1935114bcdf
Josh Bello <Jb230913@live.com>
2026-09-04 00:32:55 +0100

    update - add logic for store,edit and update

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\ContactsService;
+use App\Models\Contact;
+use App\Http\Requests\UpdateContactRequest;
+use App\Http\Requests\CreateContactRequest;
 class ContactController extends Controller
 {   
      protected ContactsService $contactService;
      public function __construct(ContactsService $contactsService){
 
       $this->contactService = $contactsService;
- }
+    }
 
     /**
      * Display a listing of the resource.
@@ -33,18 +36,28 @@ class ContactController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit()
+    public function edit(Contact $contact)
     {
-        return view('contacts.edit');
+        return view('contacts.edit', [
+        'contact' => $contact,
+    ]);
     }
 
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateContactRequest $request)
     {
-        //
+        $newUser = $this->contactService->createContact($request->validated());
+
+        if (!$newUser->id) {
+            return  redirect(route('admin.contact.new'))
+                ->with('status', false)->with('message', "New Contact didn't save, try again please");;
+        }
+        return  redirect(route('admin.contacts.index'))
+                    ->with('status', true)
+                    ->with('message', "Registration successfully");;
     }
 
     /**
@@ -59,9 +72,18 @@ class ContactController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateContactRequest $request, string $id)
     {
-        //
+
+        $updatedUser = $this->contactService
+                    ->updateContact($request->validated(),
+                                    (int)$id);
+        
+     
+            return redirect()->
+                    route('admin.contacts.index')
+                    ->with('success', "Contact was updated");
+
     }
 
     /**
