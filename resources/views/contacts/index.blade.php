d5eaf4f6dda1d1f72b4584d09978e9efcc3fb9f1
Josh Bello <Jb230913@live.com>
2026-09-03 22:51:11 +0100

    update - removed commented html code and insert directive for display contacts/filtered contacts

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,221 +1,106 @@
 @extends('layouts.admin-layout')
-    @push('styles')
-    <link rel="stylesheet" href=" {{ URL::asset('css/contact.css') }}">
-    @endpush
-    @section('title','Contact Book')
-    @section('content')
+@push('styles')
+<link rel="stylesheet" href=" {{ URL::asset('css/contact.css') }}">
+@endpush
+@section('title','Contact Book')
+@section('content')
 
-    <h1 class="h3 mt-3 fw-normal text-center"> My Contacts</h1>
-    {{--<div class="row">
-        <div class="d-flex full-height">
-            <!-- Sidebar -->
-            @include('include.sidebar')
-            <!-- Main Content -->
-            <section class="flex-grow-1 p-4 bg-body-tertiary">
+<h1 class="h3 mt-3 fw-normal text-center"> My Contacts</h1>
+<div class="container-fluid">
+    <div class="row">
 
+        <!-- Main content -->
+        <section class="col-md-9 mx-sm-auto col-lg-10 px-md-4">
 
-                <div class="row g-4">
-                    <form action="/register" method="POST">
-
-                        <div class="row">
-                            <div class="col-2 mb-3">
-                                <input type="text" class="form-control" name="first_name" id="" placeholder="first Name">
-                            </div>
-
-                            <div class="col-2 mb-3">
-                                <input type="text" class="form-control" name="lasst_name" id="" placeholder="Last Name">
-                            </div>
-                            <div class="col-2 mb-3">
-                                <input type="text" class="form-control" name="lasst_name" id="" placeholder="email">
-                            </div>
-                            <div class="col-1  | form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    Twitter
-                                </label>
-                            </div>
-                            <div class="col-1 ml-1 | form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    LinkedIn
-                                </label>
-                            </div>
-                            <div class="col-1 ml-1 | form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    Facebook
-                                </label>
-                            </div>
-                            <div class="col-1 ml-1 | form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    Favourite
-                                </label>
-                            </div>
-                        </div>
-
-                    </form>
+            <div class="row">
+                <div class="my-4">
+                    <a class="col-sm-2 btn btn-primary" href="{{ route('admin.contact.new') }}">New Contact</a>
+                </div>
+                <form action={{ route('admin.contacts.index') }} method="GET">
 
                     <div class="row">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Larry the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </section>
-        </div>
-    </div>
-    </div>--}}
-    <div class="container-fluid">
-        <div class="row">
-
-            <!-- Sidebar -->
-            {{--<nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar " id="sidebar-menu">
-                <div class="position-sticky pt-3">
-
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">
-                                Dashboard
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Orders
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Products
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-            </nav>--}}
-         
-            <!-- Main content -->
-            <section class="col-md-9 mx-sm-auto col-lg-10 px-md-4">
-
-                <div class="row">
-                    <div class="my-4">
-                        <a class="col-sm-2 btn btn-primary" href="{{ route('admin.contact.new') }}">New Contact</a>
-                    </div>
-                    <form action="/register" method="POST">
-
-                        <div class="row">
-                            <div class="col-2 mb-3">
-                                <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first Name">
-                            </div>
-
-                            <div class="col-2 mb-3">
-                                <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name">
-                            </div>
-                            <div class="col-2 mb-3">
-                                <input type="text" class="form-control" name="email" id="email" placeholder="email">
-                            </div>
-                            <div class="col-1  | form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    Twitter
-                                </label>
-                            </div>
-                            <div class="col-1 ml-1 | form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    LinkedIn
-                                </label>
-                            </div>
-                            <div class="col-1 ml-1 | form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    Facebook
-                                </label>
-                            </div>
-                            <div class="col-1 ml-1 | form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                                <label class="form-check-label" for="flexCheckChecked">
-                                    Favourite
-                                </label>
-                            </div>
+                        <div class="col-12 col-md-2 mb-3">
+                            <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first Name">
                         </div>
 
-                    </form>
+                        <div class="col-12 col-md-2 mb-3">
+                            <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name">
+                        </div>
+                        <div class="col-12 col-md-2 mb-3">
+                            <input type="text" class="form-control" name="email" id="email" placeholder="email">
+                        </div>
+                        <div class="col-12 col-md-2 mb-3 | form-check">
+                            <input class="form-check-input" type="checkbox" name="twitter" value="1" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Twitter
+                            </label>
+                        </div>
+                        <div class="col-12 col-md-2 mb-3 | form-check">
+                            <input class="form-check-input" type="checkbox" name="linkedin" value="1" id="flexCheckChecked">
+                            <label class="form-check-label" for="flexCheckChecked">
+                                LinkedIn
+                            </label>
+                        </div>
+                        <div class="col-12 col-md-2 mb-3 | form-check">
+                            <input class="form-check-input" type="checkbox" name="facebook" value="1" id="flexCheckChecked">
+                            <label class="form-check-label" for="flexCheckChecked">
+                                Facebook
+                            </label>
+                        </div>
+                        <div class="col-12 col-md-2 mb-3 | form-check">
+                            <input class="form-check-input" type="checkbox" name="favourite" value="1" id="flexCheckChecked">
+                            <label class="form-check-label" for="flexCheckChecked">
+                                Favourite
+                            </label>
+                        </div>
+                        <div class="col-12 col-md-2 mb-3 | form-check">
+                            <button type="submit" class="btn btn-primary mt-6 | text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5">
+                                Search
+                            </button>
+                        </div>
+                        <div class="col-12 col-md-2 mb-3 | form-check">
+                            <a class="btn btn-secondary" href={{ route('admin.contacts.index') }}> Reset</a>
+                        </div>
+                    </div>
 
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Mobile</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td></td>
-                                <td>
-                                    <a class="btn btn-secondary" href={{ route('admin.contact.edit',4) }}> Edit</a>
-                                    {{-- <a class="btn btn-danger" href="href="{{ route('delete_contact') }}""> Delete</a> --}}
+                </form>
 
-                                    {{-- <form action="{{ route('delete_contact') }}" method="POST" style="display:inline;">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this Contact?')">Delete</button>
-                                    </form> --}}
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                <table class="table mt-4">
+                    <thead>
+                        <tr>
+                            {{-- <th scope="col">#</th> --}}
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($data['contacts'] as $key => $value)
+                        <tr>
+                            <td colspan="1">{{ $value->first_name }}</td>
+                            <td colspan="1">{{ $value->last_name }}</td>
+                            <td colspan="1">{{ $value->email }}</td>
+                            <td colspan="1">
+                                <a class="btn btn-secondary" href={{ route('admin.contact.edit',$value->id) }}> Edit</a>
+                                <form action="{{ route('admin.contact.delete',$value->id) }}" method="POST" style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this Contact?')">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
 
-            </section>
+                    </tbody>
 
-        </div>
+                </table>
+            </div>
+            <div class="d-flex justify-content-center mt-4">
+                {{ $data['contacts']->links() }}
+            </div>
+        </section>
+
     </div>
-    @endsection
-
+</div>
+@endsection
