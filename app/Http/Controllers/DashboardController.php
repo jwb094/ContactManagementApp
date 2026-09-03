0051c48686dcb68586897de5968eda569b04af96
Josh Bello <Jb230913@live.com>
2026-09-03 18:13:04 +0100

    update - removed class import

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 //use App\Models\Tag;
-
+use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     protected  User $User;
