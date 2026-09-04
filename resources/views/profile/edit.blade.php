ae7f8586b64b5031d7153b540daf445e94c50d23
Josh Bello <Jb230913@live.com>
2026-09-04 22:22:23 +0100

    update - change hardcoded url to route() and added @method + @csrf token + removed unneeded code for v1

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -13,57 +13,30 @@
             <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <section class="form-signin col-md-9 m-auto">
                     <h1 class="h2 mb-3 fw-normal text-center my-4"> Profile Information   </h1>
-                    <form action="/update" method="POST" class="profile_information" id="profile_information">
-                        <div class="d-flex align-items-center my-2  column-gap-3">
-                            <label class="" for="avatar">Photo</label>
-                            <input  class="form-control"  type="file" name="avatar" id="avatar">
-                        </div>
+                    <form action={{ route('admin.profile.update',$user->id) }} method="POST" class="profile_information" id="profile_information">
+                        @csrf
+                        @method('PUT')
                         <div class="form-floating my-2">
-                            <input type="text" class="form-control" id="user_first_name" name="first_name" placeholder="Jonthan">
+                            <input type="text" class="form-control" id="user_first_name" name="first_name" value={{ old('last_name',$user->first_name) }}>
                             <label for="user_first_name">First Name</label>
                         </div>
                         <div class="form-floating my-2">
-                            <input type="text" class="form-control" id="user_last_name" name="last_name" placeholder="Luthor">
+                            <input type="text" class="form-control" id="user_last_name" name="last_name" value={{ old('last_name',$user->last_name) }}>
                             <label for="user_last_name">Last Name</label>
                         </div>
                         <div class="form-floating my-2">
-                            <input type="email" class="form-control" id="user_email" name="email" placeholder="name@example.com">
+                            <input type="email" class="form-control" id="user_email" name="email" value={{ old('last_name',$user->email) }}>
                             <label for="user_email">Email address</label>
                         </div>
                         <div class="form-floating my-2">
 
-                            <input type="password" class="form-control" id="user_password" name="password" placeholder="Password">
+                            <input type="password" class="form-control" id="user_password" name="password" value={{ old('last_name',$user->password) }}>
                             <label for="user_password">Password</label>
                         </div>
                         <button class="btn btn-primary w-100 py-2 mt-2" type="submit">Sign in</button>
-
                     </form>
-                        {{-- {Password} --}}
-                         <h1 class="h3 mb-3 fw-normal text-center my-4">Change Password   </h1>
-                    <form action="/update" method="POST" class="profile_password" id="profile_password">
-                       
-                        <div class="form-floating">
-
-                            <input type="password" class="form-control" id="user_password" name="password" placeholder="Password">
-                            <label for="user_password">Current Password</label>
-                        </div>
-                        <div class="form-floating my-4">
-
-                            <input type="password" class="form-control" id="user_password" name="password" placeholder="Password">
-                            <label for="user_password">New Password</label>
-                        </div>
-                        <button class="btn btn-primary w-100 py-2 mt-2" type="submit">Save</button>
-                    </form>
-                    <div class="mt-5">
-                            <div class="card" style="width: 18rem;">
-                            <div class="card-body">
-                                <h5 class="card-title">Account Information   </h5>
-                                <h6 class="card-subtitle mb-2 text-body-secondary">Role: Administrator      </h6>
-                                <p class="card-text">Status: Active</p>
-                                <p class="card-text">Member Since: 13 August 2026</p>
-                            </div>
-                            </div>
-                    </div>
+      
+        
                 </section>
             </div>
         </div>
