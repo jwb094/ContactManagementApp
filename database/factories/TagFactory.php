c7622a1f38538e1c120a4815e7945595063b5057
Josh Bello <Jb230913@live.com>
2026-09-04 17:11:40 +0100

    add new file for Model Factory Seeders for section tags

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
+ */
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        $tagNames = [
+            //Personal Circles
+            "Friends",
+            "Family",
+            "Immediate-Family",
+            "Close-Friends",
+            //Professional
+            "Co-worker",
+            "Mentor",
+            "Ex-colleague",
+            "Industry Peer",
+            //Hobbies
+            "Sports",
+            "book-club",
+            "Hiking-group",
+            "Martial-Arts Group",
+            "Football-team",
+            //Local Services / Helpers
+            "Plumber",
+            "Therapist",
+            "Doctor",
+            "Electrician",
+            "Mechanic",
+            "Pet-Vet"
+
+        ];
+        $name = $this->faker->unique()->randomElement($tagNames);
+
+        return [
+            'name' => $name
+        ];
+    }
+}
