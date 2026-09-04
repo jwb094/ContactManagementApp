bacdc8439c9953746b59a100955e9e347aaceafa
Josh Bello <Jb230913@live.com>
2026-09-04 20:11:24 +0100

    update - removal of commented tags and included need @csrf token

--- resources/views/tags/add.blade.php
+++ resources/views/tags/add.blade.php
@@ -1,57 +1,31 @@
 @extends('layouts.admin-layout')
-    @push('styles')
+@push('styles')
 
-    @endpush
-    @section('title','New Tag')
-    @section('content')
+@endpush
+@section('title','New Tag')
+@section('content')
 
-    <div class="container-fluid">
-        <div class="row">
+<div class="container-fluid">
+    <div class="row">
 
-            <!-- Sidebar -->
-            {{--<nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar " id="sidebar-menu">
-                <div class="position-sticky pt-3">
 
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">
-                                Dashboard
-                            </a>
-                        </li>
+        <!-- Main content -->
+        <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Orders
-                            </a>
-                        </li>
+            <section class="form-signin w-50 m-auto">
+                <form action={{ route('admin.tags.store') }} method="POST">
+                    @csrf
+                    <h1 class="h3 mb-3 fw-normal text-center"> New Tag</h1>
+                    <div class="form-floating">
+                        <input type="text" name="name" class="form-control" id="tagName" placeholder="Friends">
+                        <label for="tagName">Name</label> 
+                    </div>
+                    <button class="btn btn-primary w-100 py-2 mt-2" type="submit">Save</button>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Products
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-            </nav>--}}
-            @include('include.sidebar_menu')
-
-            <!-- Main content -->
-            <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-                <section class="form-signin w-50 m-auto">
-                    <form action={{ route('admin.tags.store') }} method="POST">
-                        <h1 class="h3 mb-3 fw-normal text-center"> New Tag</h1>
-                        <div class="form-floating">
-                            <input type="text" name="name" class="form-control" id="tagName" placeholder="Friends">
-                            <label for="tagName">Name</label> </div>
-                        <button class="btn btn-primary w-100 py-2 mt-2" type="submit">Sign in</button>
-
-                    </form>
-                </section>
-            </div>
-
+                </form>
+            </section>
         </div>
-    </div>
-    @endsection
 
+    </div>
+</div>
+@endsection
--- resources/views/tags/edit.blade.php
+++ resources/views/tags/edit.blade.php
@@ -8,45 +8,21 @@
     <div class="container-fluid">
         <div class="row">
 
-            <!-- Sidebar -->
-            {{--<nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar " id="sidebar-menu">
-                <div class="position-sticky pt-3">
-
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">
-                                Dashboard
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Orders
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Products
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-            </nav>--}}
-            @include('include.sidebar_menu')
-
             <!-- Main content -->
-            <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div class="col-xs-12 col-md-12 mx-sm-auto col-lg-10 px-md-4">
 
-                <section class="form-signin w-50 m-auto">
-                    <form action={{ route('admin.tags.update',2) }} method="POST">
-                        @method('put')
-                        <h1 class="h3 mb-3 fw-normal text-center"> Edit Tag</h1>
+                <section class=" m-auto">
+                    <form action={{ route('admin.tags.update',$tag) }} method="POST">
+                        @csrf
+                        @method('PUT')
+                        <h1 class="h3 mb-3 fw-normal text-center"> Edit {{ $tag->name }} Tag</h1>
                         <div class="form-floating">
-                            <input type="text" name="name" class="form-control" id="tagName" placeholder="Friends">
-                            <label for="tagName">Name</label> </div>
-                        <button class="btn btn-primary w-100 py-2 mt-2" type="submit">Sign in</button>
+                            <input type="text" name="name" class="form-control" id="name" name="name"
+                                value={{  $tag->name }}>
+                            <label for="tagName">Name</label> 
+                        </div>
+
+                        <button class="btn btn-primary w-100 py-2 mt-2" type="submit">Update</button>
 
                     </form>
                 </section>
