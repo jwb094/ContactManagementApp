13dd9be6d452ba2ed1f61b0fcff16f3a7ad8cc8a
Josh Bello <Jb230913@live.com>
2026-09-05 23:05:06 +0100

    updated - ImportData() logic added

--- app/Services/CSVService.php
+++ app/Services/CSVService.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Services;
-
 use App\Models\Contact;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Schema;
 
-class CSVService{
+class CSVService
+{
 
-    public function ExportDatafields(){
-              return Schema::getColumnListing('contacts');
+    public function ExportDatafields()
+    {
+        return Schema::getColumnListing('contacts');
     }
 
 
@@ -50,9 +52,31 @@ class CSVService{
 
             fclose($handle);
         }, $downloadFileName);
-   
     }
 
-    public function ImportData(){}
+    public function ImportData(UploadedFile $importFile)
+    {
+
+        $databaseQueryCount = 0;
+        
+        //Get the Headers(first row) from the CSV/XLS/XLSX
+        $csvFile = fopen($importFile->getRealPath(), 'r');
+        
+        $headers = fgetcsv($csvFile);
+
+        
+        while (($getData = fgetcsv($csvFile, 10000, ",")) !== FALSE){    
+                $data = array_combine($headers, $getData);
+            
+                Contact::create($data);
+
+                $databaseQueryCount++;
+        }
+        fclose($csvFile);
+
+        return   $databaseQueryCount;
+    }
+
+
 
 }
