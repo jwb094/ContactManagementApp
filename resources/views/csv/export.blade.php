7741f9bbcde87b50430875ddb0d7ca9c6aa314a5
Josh Bello <Jb230913@live.com>
2026-09-05 21:20:41 +0100

    update - removal of commented HTML tags

--- resources/views/csv/export.blade.php
+++ resources/views/csv/export.blade.php
@@ -1,60 +1,50 @@
 @extends('layouts.admin-layout')
-    @push('styles')
-    <link rel="stylesheet" href=" {{ URL::asset('css/form.css') }}">
-    @endpush
-    @section('title','Export Contact Database')
-    @section('content')
+@push('styles')
+<link rel="stylesheet" href=" {{ URL::asset('css/form.css') }}">
+@endpush
+@section('title','Export Contact Database')
+@section('content')
 
-
-    {{-- <div class="container-fluid">
-        <div class="row">
-
-            @include('include.sidebar_menu')
-
-            <!-- Main content -->
-            <div class="col-md-9 mx-sm-auto col-lg-10 px-md-4">
-                <section class="form-signin w-100 m-auto">
-
-                </section>
-            </div>
-        </div>
-    </div> --}}
-        <div class="container-fluid">
-        <div class="row">
+<div class="container-fluid">
+    <div class="row">
 
 
 
-            <!-- Main content -->
-            <section class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <!-- Main content -->
+        <section class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
 
-                <h1 class="h2">Export</h1>
+            <h1 class="h2">Export</h1>
 
-                <div class="row g-4">
-                    <div class="col-md-12">
-                        <div class="card shadow-sm">
-                            <div class="card-body">
-                          
-
-                                <form action={{ route('admin.csv.export') }} method="POST">
-                                    
-                                    <div class="mb-3">
-                                    <label for="exampleFormControlInput1" class="form-label">File Name</label>
-                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-                                    </div>
+            <div class="row g-4">
+                <div class="col-md-12">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
 
 
-                                    <button type="submit" class="btn btn-primary btn-sm"> <i class="bi bi-upload"></i>Export</button>
-                                </form>
+                            <form action={{ route('admin.csv.export') }} method="POST">
+                                @csrf
+                                <div class="mb-3">
+                                    <x-form.form-label for="" class="form-label"> File Name</x-form.form-label>
+                                    <x-form.form-input type="text" class="form-control" name="file_name" placeholder="contacts_000"></x-form.form-input>
+                                </div>
+                                <div class="mb-3">
+                                    @foreach($attributes as $key => $value)
+                                    <x-form.form-checkbox name="fields[]">{{ $value }} </x-form.form-checkbox>
+                                    @endforeach
+                                </div>
 
-                            </div>
+
+                                <button type="submit" class="btn btn-primary btn-sm"> <i class="bi bi-upload"></i>Export</button>
+                            </form>
+
                         </div>
                     </div>
                 </div>
+            </div>
 
-            </section>
+        </section>
 
-        </div>
     </div>
+</div>
 
-    @endsection
-
+@endsection
