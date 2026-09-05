89f92a120418064e5ef210120028964b14aefe27
Josh Bello <Jb230913@live.com>
2026-09-05 21:40:14 +0100

    update - add logic to export and import Contct Model class

--- app/Services/CSVService.php
+++ app/Services/CSVService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Contact;
 use Illuminate\Support\Facades\Schema;
 
 class CSVService{
@@ -13,8 +14,43 @@ class CSVService{
 
 
 
-    public function ExportData(){
-        
+    public function export(array $fields, string $fileName)
+    {
+
+        $downloadFileName = $fileName . ".csv";
+
+        $contacts = Contact::select($fields)->get();
+
+        $csvHeaders = array_map(
+            fn($field) => ucwords(str_replace('_', ' ', $field)),
+            $fields
+        );
+
+
+        return response()->streamDownload(function () use (
+            $contacts,
+            $fields,
+            $csvHeaders
+        ) {
+            $handle = fopen('php://output', 'w');
+
+            // CSV header
+            fputcsv($handle, $csvHeaders);
+
+            // CSV rows
+            foreach ($contacts as $contact) {
+                $row = [];
+
+                foreach ($fields as $field) {
+                    $row[] = $contact->{$field};
+                }
+
+                fputcsv($handle, $row);
+            }
+
+            fclose($handle);
+        }, $downloadFileName);
+   
     }
 
     public function ImportData(){}
