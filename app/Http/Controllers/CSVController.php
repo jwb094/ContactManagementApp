eef741f07d3024cf6d2709c8165cd6476d8782aa
Josh Bello <Jb230913@live.com>
2026-09-05 23:06:01 +0100

    update - csv_import() updated with logic and DI and class import for controller

--- app/Http/Controllers/CSVController.php
+++ app/Http/Controllers/CSVController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers;
 use App\Http\Requests\ExportContactsRequest;
+use App\Http\Requests\ImportContactRequest;
 use App\Services\CSVService;
-
 class CSVController extends Controller
 {
 
-     protected CSVService $CSVService;
+    protected CSVService $CSVService;
 
-     public function __construct(CSVService $CSVService){
+    public function __construct(CSVService $CSVService)
+    {
 
-      $this->CSVService = $CSVService;
+        $this->CSVService = $CSVService;
     }
     /**
      * Display a listing of the resource.
@@ -25,9 +26,9 @@ class CSVController extends Controller
      * Display a CSV export PAge of the resource.
      */
     public function contact_export()
-    {   
+    {
         $fields = $this->CSVService->ExportDatafields();
-        return view('csv.export',['attributes' => $fields]);
+        return view('csv.export', ['attributes' => $fields]);
     }
 
 
@@ -35,19 +36,31 @@ class CSVController extends Controller
     /**
      * Import data from CSV file into database
      */
-    public function csv_import() {
+    public function csv_import(ImportContactRequest $request)
+    {
+        $validated = $request->validated();
 
+        $importFile = $validated['import_data_file'];
 
+        $databaseQueryCount = $this->CSVService->ImportData($importFile);
+
+        return redirect()
+            ->back()
+            ->with(
+                'success',
+                $databaseQueryCount . ' records were inserted into the Database.'
+            );
     }
 
     /**
      * Export data from CSV file into database
      */
-    public function csv_export(ExportContactsRequest $request) {
+    public function csv_export(ExportContactsRequest $request)
+    {
 
         $formValidatedColums = $request->validated('fields');
         $formValidatedFileName = $request->validated('file_name');
 
-        return $this->CSVService->export($formValidatedColums,$formValidatedFileName);
+        return $this->CSVService->export($formValidatedColums, $formValidatedFileName);
     }
 }
