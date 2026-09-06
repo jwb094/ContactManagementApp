89ee34ff84001f19a86e4fe367690a1a5617812b
Josh Bello <Jb230913@live.com>
2026-09-06 10:03:34 +0100

    upupdate - change hardcoded form elements to component form elements

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,47 +1,46 @@
 @extends('layouts.admin-layout')
-    @push('styles')
+@push('styles')
 
-    @endpush
-    @section('title','Edit Profile')
-    @section('content')
+@endpush
+@section('title','Edit Profile')
+@section('content')
 
-    <div class="container-fluid">
-        <div class="row">
+<div class="container-fluid">
+    <div class="row">
 
 
-            <!-- Main content -->
-            <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <section class="form-signin col-md-9 m-auto">
-                    <h1 class="h2 mb-3 fw-normal text-center my-4"> Profile Information   </h1>
-                    <form action={{ route('admin.profile.update',$user->id) }} method="POST" class="profile_information" id="profile_information">
-                        @csrf
-                        @method('PUT')
-                        <div class="form-floating my-2">
-                            <input type="text" class="form-control" id="user_first_name" name="first_name" value={{ old('last_name',$user->first_name) }}>
-                            <label for="user_first_name">First Name</label>
-                        </div>
-                        <div class="form-floating my-2">
-                            <input type="text" class="form-control" id="user_last_name" name="last_name" value={{ old('last_name',$user->last_name) }}>
-                            <label for="user_last_name">Last Name</label>
-                        </div>
-                        <div class="form-floating my-2">
-                            <input type="email" class="form-control" id="user_email" name="email" value={{ old('last_name',$user->email) }}>
-                            <label for="user_email">Email address</label>
-                        </div>
-                        <div class="form-floating my-2">
+        <!-- Main content -->
+        <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <section class="form-signin col-md-9 m-auto">
+                <h1 class="h2 mb-3 fw-normal text-center my-4"> Profile Information </h1>
+                <form action={{ route('admin.profile.update',$user->id) }} method="POST" class="profile_information" id="profile_information">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-check mt-3">
+                        <x-form.form-label for="first_name" class="form-label"> First Name </x-form.form-label>
+                        <x-form.form-input type="text" id="first_name" class="form-control" name="first_name" value="{{ old('first_name',$user->first_name) }}"></x-form.form-input>
+                    </div>
+                    <div class="form-check mt-3">
+                        <x-form.form-label for="user_last_name" class="form-label"> Last Name </x-form.form-label>
+                        <x-form.form-input type="text" id="user_last_name" class="form-control" name="last_name" value="{{ old('last_name',$user->last_name) }}"></x-form.form-input>
+                    </div>
+                    <div class="form-check mt-3">
+                        <x-form.form-label for="user_email" class="form-label"> Last Name </x-form.form-label>
+                        <x-form.form-input type="text" id="user_email" class="form-control" name="email" value="{{ old('user_email',$user->email) }}"></x-form.form-input>
+                    </div>
+                    <div class="form-check mt-3">
+                        <x-form.form-label for="user_password" class="form-label"> Password </x-form.form-label>
+                        <x-form.form-input type="password" id="user_password" class="form-control" name="password" value="{{ old('user_password',$user->password) }}"></x-form.form-input>
+                    </div>
+                    <button class="btn btn-primary w-100 py-2 mt-2" type="submit">Update</button>
+                </form>
 
-                            <input type="password" class="form-control" id="user_password" name="password" value={{ old('last_name',$user->password) }}>
-                            <label for="user_password">Password</label>
-                        </div>
-                        <button class="btn btn-primary w-100 py-2 mt-2" type="submit">Sign in</button>
-                    </form>
-      
-        
-                </section>
-            </div>
+
+            </section>
         </div>
     </div>
+</div>
 
 
 
-    @endsection
+@endsection
