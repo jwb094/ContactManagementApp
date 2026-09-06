391c7591cd30f89f5947c818b9d0527837d2bc1c
Josh Bello <Jb230913@live.com>
2026-09-06 08:31:08 +0100

    update - insert components form elements in Social Media details form section and remove hardcoded duplicate form elements

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -105,21 +105,21 @@
 
                         <div class="step">
                             <h2>Social Media Handle </h2>
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="facebook" name="facebook" value="{{ old('facebook') }}">
-                                <label for="facebook" class="form-label">Facebook </label>
+                            <div class="form-check mt-3">
+                                <x-form.form-label for="facebook" class="form-label"> Facebook </x-form.form-label>
+                                <x-form.form-input type="text" id="facebook" class="form-control" name="facebook" value="{{ old('facebook') }}"></x-form.form-input>
                             </div>
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="twitter" name="twitter" value="{{ old('twitter') }}">
-                                <label for="twitter" class="form-label">X(formerly known as Twitter)</label>
+                            <div class="form-check mt-3">
+                                <x-form.form-label for="twitter" class="form-label"> X(formerly known as Twitter) </x-form.form-label>
+                                <x-form.form-input type="text" id="twitter" class="form-control" name="twitter" value="{{ old('twitter') }}"></x-form.form-input>
                             </div>
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="instagram" name="instagram" value="{{ old('instagram') }}">
-                                <label for="instagram" class="form-label">Instagram</label>
+                            <div class="form-check mt-3">
+                                <x-form.form-label for="instagram" class="form-label"> Instagram </x-form.form-label>
+                                <x-form.form-input type="text" id="instagram" class="form-control" name="instagram" value="{{ old('instagram') }}"></x-form.form-input>
                             </div>
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="linkedin" name="linkedin" value="{{ old('linkedin') }}">
-                                <label for="linkedin" class="form-label">LinkedIn</label>
+                            <div class="form-check my-3">
+                                <x-form.form-label for="linkedin" class="form-label"> Linkedin </x-form.form-label>
+                                <x-form.form-input type="text" id="linkedin" class="form-control" name="linkedin" value="{{ old('linkedin') }}"></x-form.form-input>
                             </div>
                         </div>
                         <div class="buttons">
