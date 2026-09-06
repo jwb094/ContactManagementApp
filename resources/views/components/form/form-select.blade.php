021cff7baf4d7b30e0501f712dc40d7da7a8d134
Josh Bello <Jb230913@live.com>
2026-09-06 09:01:38 +0100

    update logic slected value for form-select

--- resources/views/components/form/form-select.blade.php
+++ resources/views/components/form/form-select.blade.php
@@ -1,13 +1,15 @@
-<select {{ $attributes->merge(['type' =>''
+<select 
+{{-- {{ $attributes->merge(['type' =>''
 ,'class' =>''
 ,'name' =>''
 ,'id' =>''
-,'value'=> ""]) }}>
-    <option value="Mr">Mr</option>
-    <option value="Mrs">Mrs</option>
-    <option value="Ms">Ms</option>
-    <option value="Master">Master</option>
-    <option value="Dr">Dr</option>
-    <option value="Prof">Professor</option>
-    <option value="Sir">Sir</option>
+,'value'=> ""]) }} --}}
+{{ $attributes->except('value') }}>
+    <option value="Mr" @selected($value === 'Mr')>Mr</option>
+    <option value="Mrs" @selected($value ==='Mrs')>Mrs</option>
+    <option value="Ms" @selected($value ==='Ms')>Ms</option>
+    <option value="Master" @selected($value ==='Master')>Master</option>
+    <option value="Dr" @selected($value ==='Dr')>Dr</option>
+    <option value="Prof" @selected($value ==='Prof')>Professor</option>
+    <option value="Sir" @selected($value === 'Sir')>Sir</option>
 </select>
