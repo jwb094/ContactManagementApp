77787b743118bd20aea0ed80d02e5748f567b9b2
Josh Bello <Jb230913@live.com>
2026-09-06 07:26:29 +0100

    update - insert components form to repeat hardcoded form input

--- resources/views/tags/add.blade.php
+++ resources/views/tags/add.blade.php
@@ -16,9 +16,9 @@
                 <form action={{ route('admin.tags.store') }} method="POST">
                     @csrf
                     <h1 class="h3 mb-3 fw-normal text-center"> New Tag</h1>
-                    <div class="form-floating">
-                        <input type="text" name="name" class="form-control" id="tagName" placeholder="Friends">
-                        <label for="tagName">Name</label> 
+                    <div class="form-check mt-3">
+                        <x-form.form-label for="tagname" class="form-label"> Tag Name</x-form.form-label>
+                        <x-form.form-input type="text" id="tagname" class="form-control" name="name" placeholder="Friends"></x-form.form-input>
                     </div>
                     <button class="btn btn-primary w-100 py-2 mt-2" type="submit">Save</button>
 
